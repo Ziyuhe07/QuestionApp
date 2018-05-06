0bad6b2f1511e4ba2ee119458ec14d6384f9bf84
Ziyehe <ziyu.he99@gmail.com>
2018-05-05 21:12:22 -0400

    Insert data

--- ucfazhe/httpServer.js
+++ ucfazhe/httpServer.js
@@ -79,9 +79,7 @@ app.use(bodyParser.json());
 				var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")'";
 				
 				var querystring = "INSERT into questionform (name,question,possibleanswer1,possibleanswer2,possibleanswer3, possibleanswer4,rightanswer,geom) values ('";
-				querystring = querystring + req.body.name + "','" + req.body.question + "','" 
-				+ req.body.possibleanswer1 + "','"+ req.body.possibleanswer2 + "','"+ req.body.possibleanswer3 + "','"+ req.body.possiblenswer4 + "','"
-				+ req.body.rightAnswer + "','";
+				querystring = querystring + req.body.name + "','" + req.body.question + "','" + req.body.possibleanswer1 + "','"+ req.body.possibleanswer2 + "','"+ req.body.possibleanswer3 + "','"+ req.body.possiblenswer4 + "','"+ req.body.rightAnswer + "','";
 				console.log(querystring);
 				client.query( querystring,function(err,result) {
 					done();
