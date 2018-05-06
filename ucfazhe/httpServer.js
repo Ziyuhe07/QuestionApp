a2e3d8645d8e7230117bcb8e7d2d2af93baabdef
Ziyehe <ziyu.he99@gmail.com>
2018-05-05 23:12:52 -0400

    Insert to database

--- ucfazhe/httpServer.js
+++ ucfazhe/httpServer.js
@@ -78,9 +78,8 @@ app.use(bodyParser.json());
 				// well known text should look like: 'POINT(-71.064544 42.28787)'
 				var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")'";
 				
-				var querystring = "INSERT into formdata (name,surname,module,language, modulelist, lecturetime, geom) values ('";
-				querystring = querystring + req.body.name + "','" + req.body.surname + "','" + req.body.module + "','";
-				querystring = querystring + req.body.language + "','" + req.body.modulelist + "','" + req.body.lecturetime+"',"+geometrystring+ "))";
+				var querystring = "INSERT into questionform (name,question,answera,answerb,answerc, answerd,ranswer,geom) values ('";
+				querystring = querystring + req.body.name + "','" + req.body.question + "','" + req.body.answera + "','"+ req.body.answerb + "','"+ req.body.answerc + "','"+ req.body.answerd + "','"+ req.body.ranswer + "',"+geometrystring+ "))";
 				console.log(querystring);
 				client.query( querystring,function(err,result) {
 					done();
